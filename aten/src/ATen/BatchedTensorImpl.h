bf79521620630d7a7871ac45c29927a117d7d100
Richard Zou <zou3519@gmail.com>
2020-05-21 14:10:20 -0700

    Add "batching rule" for torch.sum(tensor, dims)
    On the way to moving vmap prototype into tree, this is the
    implementation of the sum batching rule and some utility functions.
    After implementing this and another batching rule (probably for
    at::mul), we'll be ready to for the initial vmap Python
    API.
    
    Batching rules and utility functions act on "unpacked BatchedTensors".
    This is to avoid tensor allocation overhead (one alternative is having
    these act on BatchedTensors). Unfortunately this leads to more verbose
    and confusing code. I've considered adding a special
    "BatchedTensorData" struct that only holds a Tensor and BatchDims and
    has helper methods defined on it but I'm not sure if that is better.
    
    Re: testing: There's a bit of a chicken-and-egg problem here; I'd like to test
    that the batching rules work for vmap, but the easiest way to do that is using
    the (non-existent) vmap python API. This PR includes some minimal testing for
    this batching rule.
    
    Future PRs (for more context):
    - [YOU ARE HERE] Add torch.sum(tensor, dims) batching rule + some helper functions
    - Add torch.mul batching rule + some more helper functions
    - Add vmap python API
    - Add "slow" for-loop fallbacks for out-of-place functions via
    dispatcher fallback mechanism.
    - Add batching rules for "view" functions
    - Add "slow" for-loop fallbacks for in-place functions
    - And more
    
    Test Plan:
    - `./build/bin/vmap_test`
    
    ghstack-source-id: ec5f7ff032e1540fe55bdf277b06ca0781610087
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38723

--- aten/src/ATen/BatchedTensorImpl.h
+++ aten/src/ATen/BatchedTensorImpl.h
@@ -78,6 +78,7 @@ struct TORCH_API BatchedTensorImpl : public c10::TensorImpl {
  private:
   Tensor value_;
 
+  // NOTE: [BatchDims sorted by level invariant]
   // There is an invariant that the BatchDims must be stored in increasing `level`
   // order. That is, for i < j, bdims_[i].level must be less than bdims_[j].level.
   BatchDims bdims_;
